3808e2f693f497ba83f4125f47008d118ecee55d
Eric DePree <edepree@users.noreply.github.com>
2020-07-04 12:48:56 -0500

    Update updater.py
    Updated lists merged for "research-combine.txt" and added a combine list for "port-scanners-combine.txt"

--- updater.py
+++ updater.py
@@ -4,9 +4,18 @@ import urllib.request
 import xml.etree.ElementTree as ET 
 
 def lambda_handler(event, context):
-    research_list_names = ['blindferret', 'cybergreen', 'erratasec', 'onyphe',
-                           'rapid7sonar', 'shadowserver', 'shodan', 'univmichigan']
-    research_list_ips = ''
+    research_names = ['blindferret', 'cybergreen', 'erratasec', 'onyphe',
+                      'rapid7sonar', 'shadowserver', 'shodan', 'univmichigan',
+                      'ipip', 'alphastrikelabs']
+    research_ips = ''
+
+    port_scanner_names = ['blocklistde110', 'blocklistde143', 'blocklistde21',
+                          'blocklistde22', 'blocklistde25', 'blocklistde443',
+                          'blocklistde80', 'blocklistde993', 'blocklistdeapache',
+                          'blocklistdeasterisk', 'blocklistdebots', 'blocklistdebruteforcelogin',
+                          'blocklistdecourierimap', 'blocklistdecourierpop3', 'openbl_ftp',
+                          'openbl_http', 'openbl_mail', 'openbl_smtp', 'openbl_ssh']
+    port_scanner_ips = ''
 
     s3_bucket = 'REPLACE_ME'
     s3 = boto3.client('s3')
@@ -19,8 +28,7 @@ def lambda_handler(event, context):
         if element.find('datatype').text == 'is_ipv4':
             threat_list_name = element.find('type').text
             threat_list_url = f'https://isc.sans.edu/api/threatlist/{threat_list_name}'
-            #print(f'Found {threat_list_name}')
-
+            
             html_request = urllib.request.urlopen(threat_list_url).read()
             root = ET.fromstring(html_request)
 
@@ -29,15 +37,24 @@ def lambda_handler(event, context):
                 threat_list_ips += element.find('ipv4').text + '\n'
 
             if len(threat_list_ips) > 0:
-                #print(threat_list_ips)
+                print(f'Found {threat_list_name} with one or more entry')
+
                 s3.put_object(Bucket=s3_bucket, Key=f'{threat_list_name}.txt',
                               Body=threat_list_ips, ACL='public-read')
 
-                if threat_list_name in research_list_names:
-                    research_list_ips += threat_list_ips
+                if threat_list_name in research_names:
+                    print(f'Added {threat_list_name} to research-combine')
+                    research_ips += threat_list_ips
+                elif threat_list_name in port_scanner_names:
+                    port_scanner_ips += threat_list_ips
+                    print(f'Added {threat_list_name} to port-scanners-combine')
 
     s3.put_object(Bucket=s3_bucket, Key='research-combine.txt',
-                  Body=research_list_ips, ACL='public-read')
+                  Body=research_ips, ACL='public-read')
+
+    s3.put_object(Bucket=s3_bucket, Key='port-scanners-combine.txt',
+                  Body=research_ips, ACL='public-read')
+
 
 
 #if __name__ == '__main__':
